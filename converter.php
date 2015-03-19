65019a3d16f487fdd1ea5b43f3403a4eaeb92252
Aaron Valandra <avaland@woopta.com>
2015-03-19 08:41:04 -0500

    Changed everything to based out of the second meridian.

--- converter.php
+++ converter.php
@@ -11,14 +11,10 @@ $csvWriter = Writer::createFromFileObject(new SplFileObject('TSRQUpdate.csv', 'w
 $csvData = $csv->fetchAssoc(['id', 'no', 'quad', 'tr', 't', 'td', 'r', 'rd', 's', 'q']);
 
 foreach ($csvData as $line) {
-    $meridian = 1;
-
-    if ($line['rd'] === "E")
-        $meridian = 2;
 
     $parameter = array(
         'IN',
-        $meridian,
+        2,
         str_replace(' ', '',trim($line['t'])),
         0,
         str_replace(' ', '',trim($line['td'])),
@@ -68,66 +64,8 @@ foreach ($csvData as $line) {
 
             $csvWriter->insertOne($array_of_data);
         } else {
-            //Meridian could of predicted wrongly...
-            $meridian = 2;
-
-            if ($line['rd'] === "E")
-                $meridian = 1;
-
-            $parameter = array(
-                'IN',
-                $meridian,
-                str_replace(' ', '',trim($line['t'])),
-                0,
-                str_replace(' ', '',trim($line['td'])),
-                str_replace(' ', '',trim($line['r'])),
-                0,
-                str_replace(' ', '',trim($line['rd'])),
-                str_replace(' ', '',trim($line['s'])),
-                str_replace(' ', '',trim($line['q'])),
-                0
-            );
-
-            $url = 'http://www.geocommunicator.gov/TownshipGeocoder/TownshipGeocoder.asmx/GetLatLon?TRS=' . implode(',', $parameter);
-            $document = new \SimpleXMLElement(file_get_contents($url));
-
-            if ((string)$document->Message === "Ok") {
-
-                $points = array();
-                $center_points = array();
-
-                $data = $document->Data;
-
-                $simplexml = new \SimpleXMLElement($data);
-
-                $center_points = explode(',', (string)$simplexml->channel->item[0]->description);
-
-                foreach ($center_points as $point) {
-                    preg_match("/\([^)]+\)/", $point, $output);
-                    $points[] = preg_replace('/\(|\)/', '', $output[0]);
-                }
-
-                $center_points = $points;
-
-                $document = new \DOMDocument();
-                $document->loadXML($data);
-
-                foreach ($document->getElementsByTagName('polygon') as $polygon) {
-                    if ($polygon->hasChildNodes()) {
-                        $points = explode(',', trim($polygon->firstChild->nodeValue));
-                    }
-                }
-
-                $points = array_merge($center_points, $points);
-
-                $array_of_data = array_merge($line, $points);
-
-                $csvWriter->insertOne($array_of_data);
-            } else {
-                array_push($line, $url);
-                $csvWriter->insertOne($line);
-            }
-
+            array_push($line, $url);
+            $csvWriter->insertOne($line);
         }
     } else {
         array_push($line, $url);
